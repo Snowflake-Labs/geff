7b9320a3d38bf2e578335f6f6b9912a4575e5e6c
sfc-gh-afedorov <42752788+sfc-gh-afedorov@users.noreply.github.com>
2021-08-27 10:45:34 -0700

    Update process_https.py

--- lambda_src/drivers/process_https.py
+++ lambda_src/drivers/process_https.py
@@ -57,11 +57,9 @@ def process_row(
         for k, v in (
             loads(headers)
             if headers.startswith('{')
-            else (
-                parse_header_dict(headers)
-                if headers
-                else {}
-            )
+            parse_header_dict(headers)
+            if headers
+            else {}
         ).items()
     }
 
