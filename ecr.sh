f6f41cc6356ddbf1de30bac98ffaa42a11bbef2e
Prasanth Kommini <prasanth.kommini@snowflake.com>
2021-11-03 09:10:40 -0700

    Add script to push to ECR

---
+++ ecr.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+ACCOUNT_ID="<enter account ID>"
+IMAGE_VER=geff:latest
+ECR_HOST="075393536245.dkr.ecr.us-west-2.amazonaws.com"
+
+# Create repo for GEFF docker image
+aws ecr create-repository --repository-name geff --region us-west-2
+
+# NOTE: From here all steps need docker desktop to be running.
+
+# Login
+aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin $ECR_HOST
+
+# Docker Build (Needs docker desktop running)
+docker build -t geff .
+
+docker tag $IMAGE_VER $ECR_HOST/$IMAGE_VER
+
+docker push $ECR_HOST/$IMAGE_VER
