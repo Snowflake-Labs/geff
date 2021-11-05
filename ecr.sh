b8aa0267b37cd8c3a99ab63cc0a9c6297cb35b19
Prasanth Kommini <prasanth.kommini@snowflake.com>
2021-11-05 13:59:01 -0700

    Remove ls -l

--- ecr.sh
+++ ecr.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-ACCOUNT_ID="<enter account ID>"
+ACCOUNT_ID="$1"
+AWS_REGION="$2"
 IMAGE_VER=geff:latest
-ECR_HOST="075393536245.dkr.ecr.us-west-2.amazonaws.com"
+ECR_HOST="$ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com"
 
 # Create repo for GEFF docker image
-aws ecr create-repository --repository-name geff --region us-west-2
+aws ecr create-repository --repository-name geff --region $AWS_REGION
 
 # NOTE: From here all steps need docker desktop to be running.
 
 # Login
-aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin $ECR_HOST
+aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $ECR_HOST
 
 # Docker Build (Needs docker desktop running)
 docker build -t geff .
