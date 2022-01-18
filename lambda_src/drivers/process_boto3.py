481751f3a6275c491c38d875ae069390c368314f
Andrey Fedorov <andrey.fedorov@snowflake.com>
2022-01-18 14:56:36 -0800

    Add assume role option to boto3 driver

--- lambda_src/drivers/process_boto3.py
+++ lambda_src/drivers/process_boto3.py
@@ -8,11 +8,32 @@ DISALLOWED_CLIENTS = {'kms', 'secretsmanager'}
 
 
 def process_row(
-    client_name, method_name, results_path=None, region='us-west-2', **kwargs
+    client_name,
+    method_name,
+    assume_role_arn=None,
+    role_session_name=None,
+    results_path=None,
+    region='us-west-2',
+    **kwargs,
 ):
     if client_name in DISALLOWED_CLIENTS:
         return
-    method = getattr(boto3.client(client_name, region), method_name)
+    if assume_role_arn:
+        creds = boto3.client('sts').assume_role(
+            RoleArn=assume_role_arn,
+            RoleSessionName=f'geff_{role_session_name}'
+            if role_session_name is None
+            else 'geff',
+        )
+        client = boto3.client(
+            client_name,
+            region=region,
+            aws_access_key_id=ACCESS_KEY,
+            aws_secret_access_key=SECRET_KEY,
+            aws_session_token=SESSION_TOKEN,
+        )
+    client = boto3.client(client_name, region)
+    method = getattr(client, method_name)
     result = method(**kwargs)
     if (
         results_path
