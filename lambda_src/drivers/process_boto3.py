72cf4cfcca515e9b82c7329b04d798eae4e2c8d8
Andrey Fedorov <andrey.fedorov@snowflake.com>
2022-01-18 16:40:43 -0800

    add optional eid

--- lambda_src/drivers/process_boto3.py
+++ lambda_src/drivers/process_boto3.py
@@ -20,20 +20,21 @@ def process_row(
         return
     if assume_role_arns:
         creds = None
-        for assume_role_arn in loads(assume_role_arns):
+        for arn in loads(assume_role_arns):
             access_key = creds['Credentials']['AccessKeyId'] if creds else None
             secret_key = creds['Credentials']['SecretAccessKey'] if creds else None
             aws_session_token = creds['Credentials']['SessionToken'] if creds else None
+            assume_role_params = arn if type(arn) is dict else {"RoleArn": arn}
             creds = boto3.client(
                 'sts',
                 aws_access_key_id=access_key,
                 aws_secret_access_key=secret_key,
                 aws_session_token=aws_session_token,
             ).assume_role(
-                RoleArn=assume_role_arn,
-                RoleSessionName=f'geff_{role_session_name}'
+                RoleSessionName='geff'
                 if role_session_name is None
-                else 'geff',
+                else f'geff_{role_session_name}',
+                **assume_role_params
             )
         client = boto3.client(
             client_name,
